65f4d67fdba12afc36f3e9eb08c739c434e6eda0
matibraun <matibraun@gmail.com>
2021-02-19 11:33:26 -0300

    jueguitos

---
+++ games/src/App.js
@@ -0,0 +1,12 @@
+import Mensaje from './Mensaje'
+
+function App() {
+  return (
+    <div>
+      <Mensaje contenido={'chausisis'}/>
+      <Mensaje contenido={'hoolisss'}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ games/src/Mensaje2.js
@@ -0,0 +1,14 @@
+
+
+function Mensaje(props) {
+    return (
+      <div>
+        <header>
+          {props.contenido}
+        </header>
+      </div>
+    );
+  }
+  
+  export default Mensaje;
+  
